6f29b1d24ebd55333d15f8414afc06124a9ceaf4
Prabah Prasad Kalita <76781633+prabah101@users.noreply.github.com>
2023-02-10 16:22:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,61 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+from enum import Enum
+
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+POTATO_MODEL = tf.keras.models.load_model("./saved_models/2")
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+class AvailablePlants(str, Enum):
+    tomato = "tomato"
+    potato = "potato"
+    bell_pepper = "bell_pepper"
+
+@app.get("/hello")
+async def hello():
+    return "This is a Plant Disease classification API"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict/{plant}")
+async def predict(
+    file: UploadFile = File(...),
+    plant: AvailablePlants = AvailablePlants.potato
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = POTATO_MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+    return {
+        'plant': plant,
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
 
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+# if __name__ == "__main__":
+#     uvicorn.run(app, host='localhost', port=8000)
