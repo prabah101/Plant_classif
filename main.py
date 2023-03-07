e374502d3007e73f790d2c5a82609b1903c3015e
Prabah Prasad Kalita <76781633+prabah101@users.noreply.github.com>
2023-03-07 15:17:51 +0530

    additional models added

--- main.py
+++ main.py
@@ -21,27 +21,57 @@ app.add_middleware(
 )
 
 POTATO_MODEL = tf.keras.models.load_model("./saved_models/2")
-POTATO_CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+POTATO_CLASS_NAMES = [
+    "Early Blight",
+    "Late Blight",
+    "Healthy"]
 
 BELL_PEPPER_MODEL = tf.keras.models.load_model("./saved_models/3")
-BELL_PEPPER_CLASS_NAMES = ["Bacterial Spot", "Healthy"]
+BELL_PEPPER_CLASS_NAMES = [
+    "Bacterial Spot", 
+    "Healthy"]
 
 TOMATO_MODEL = tf.keras.models.load_model("./saved_models/4")
-TOMATO_CLASS_NAMES = ["Bacterial Spot",
- "Early Blight",
- "Late Blight",
- "Leaf Mold",
- "Septoria Leaf Spot",
- "Spider Mites Two Spotted Spider Mite",
- "Target Spot",
- "Yellow Leaf Curl Virus",
- "Mosaic Virus",
- "Healthy"]
+TOMATO_CLASS_NAMES = [
+    "Bacterial Spot",
+    "Early Blight",
+    "Late Blight",
+    "Leaf Mold",
+    "Septoria Leaf Spot",
+    "Spider Mites Two Spotted Spider Mite",
+    "Target Spot",
+    "Yellow Leaf Curl Virus",
+    "Mosaic Virus",
+    "Healthy"]
+
+APPLE_MODEL = tf.keras.models.load_model("./saved_models/5")
+APPLE_CLASS_NAMES = [
+    "Apple Apple scab",
+    "Apple Black rot",
+    "Apple Cedar apple rust",
+    "Apple healthy"]
+
+GRAPE_MODEL = tf.keras.models.load_model("./saved_models/6")
+GRAPE_CLASS_NAMES = [
+    "Grape Black rot",
+    "Grape Esca (Black Measles)",
+    "Grape Leaf blight (Isariopsis Leaf Spot)",
+    "Grape healthy"]
+
+CORN_MODEL = tf.keras.models.load_model("./saved_models/7")
+CORN_CLASS_NAMES = [
+    "Corn Cercospora leaf spot Gray leaf spot",
+    "Corn Common rust",
+    "Corn Northern Leaf Blight",
+    "Corn healthy"]
 
 class AvailablePlants(str, Enum):
     tomato = "tomato"
     potato = "potato"
     bell_pepper = "bell_pepper"
+    apple = "apple"
+    grape = "grape"
+    corn = "corn"
 
 @app.get("/")
 async def hello():
@@ -68,6 +98,15 @@ async def predict(
     elif(plant == "tomato"):
         predictions = TOMATO_MODEL.predict(img_batch)
         predicted_class = TOMATO_CLASS_NAMES[np.argmax(predictions[0])]
+    elif(plant == "apple"):
+        predictions = APPLE_MODEL.predict(img_batch)
+        predicted_class = APPLE_CLASS_NAMES[np.argmax(predictions[0])]
+    elif(plant == "grape"):
+        predictions = GRAPE_MODEL.predict(img_batch)
+        predicted_class = GRAPE_CLASS_NAMES[np.argmax(predictions[0])]
+    elif(plant == "corn"):
+        predictions = CORN_MODEL.predict(img_batch)
+        predicted_class = CORN_CLASS_NAMES[np.argmax(predictions[0])]
 
     confidence = round(100 * (np.max(predictions[0])), 2)
 
